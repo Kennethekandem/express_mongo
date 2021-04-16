580a14dabb16f7e0487e5d75ca7f912c1e71f9b1
Kenneth Ekandem <kennyekandem@gmail.com>
2021-04-16 09:08:24 +0100

    update

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('dotenv/config');
 
+app.use(bodyParser.json());
+
+// post route
 const postRoute = require('./routes/posts');
-
 app.use('/posts', postRoute);
 
+// Home route
 app.get('/', (req, res) => {
     res.send("We are home");
 })
 
 mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-    console.log("connected")});
+    console.log("connected");
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
 router.get('/', (req, res) => {
     res.send('Hello world!');
+});
+
+// Create post
+router.post('/', (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+
+    post.save()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(error => {
+            res.status(400).json({msg: 'Error posting', error});
+        })
 })
 
 module.exports = router;
